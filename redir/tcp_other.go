328d938b55313d6be071fbd8aa39cc54a5d46680
ccsexyz <mofaxyz@gmail.com>
2017-03-06 22:31:45 +0800

    fix the problem that can't build under non-linux platform

--- redir/tcp_other.go
+++ redir/tcp_other.go
@@ -2,14 +2,11 @@
 
 package redir
 
-import "github.com/shadowsocks/go-shadowsocks2/core"
+import (
+	"fmt"
+	"net"
+)
 
-// copy from https://github.com/riobard/go-shadowsocks2
-
-func redirLocal(addr, server string, ciph core.StreamConnCipher) {
-	//logf("TCP redirect not supported")
-}
-
-func redir6Local(addr, server string, ciph core.StreamConnCipher) {
-	//logf("TCP6 redirect not supported")
+func GetOrigDst(conn net.Conn) (string, error) {
+	return "", fmt.Errorf("this function is only avaliable at linux")
 }
