fa29e69eafb843223123c6defdf7090247d4ff2f
ccsexyz <mofaxyz@gmail.com>
2017-03-24 02:12:37 +0800

    Revert "support udprelay in redir mode"
    This reverts commit ddb1275b010b070d2f5b6fd30de3c5f5481caff8.

--- shadowsocks/config.go
+++ shadowsocks/config.go
@@ -73,10 +73,10 @@ func CheckConfig(c *Config) {
 			c.Type = "server"
 		}
 	}
-	if c.UDPRelay && c.Type != "server" && c.Type != "local" && c.Type != "udptun" && c.Type != "multiserver" && c.Type != "redir" {
+	if c.UDPRelay && c.Type != "server" && c.Type != "local" && c.Type != "udptun" && c.Type != "multiserver" {
 		c.UDPRelay = false
 	}
-	if c.UDPOverTCP && c.Type != "server" && c.Type != "local" && c.Type != "udptun" && c.Type != "multiserver" && c.Type != "redir" {
+	if c.UDPOverTCP && c.Type != "server" && c.Type != "local" && c.Type != "udptun" && c.Type != "multiserver" {
 		c.UDPOverTCP = false
 	}
 	for _, v := range c.Backends {
