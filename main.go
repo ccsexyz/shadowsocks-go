ece8acad5e91b925cf827982b22daf2e64cfa971
ccsexyz <mofaxyz@gmail.com>
2017-04-23 16:43:41 +0800

    support command line arguments

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"os"
 	"sync"
@@ -12,11 +12,38 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("usage: myss configfile")
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime | log.Lmicroseconds)
+	if len(os.Args) != 2 || os.Args[1] == "-h" {
+		// fmt.Println("usage: myss configfile")
+		var c ss.Config
+		var target string
+		flag.StringVar(&c.Type, "type", "", "server type(eg: server, local)")
+		flag.StringVar(&c.Remoteaddr, "s", "", "remote server address")
+		flag.StringVar(&c.Localaddr, "l", "", "local listen address")
+		flag.StringVar(&target, "t", "", "target address(for tcptun and udptun)")
+		flag.StringVar(&c.Method, "m", "aes-256-cfb", "crypt method")
+		flag.StringVar(&c.Password, "p", "you need a password", "password")
+		flag.BoolVar(&c.Nonop, "nonop", false, "enable this to be compatiable with official ss servers(client only)")
+		flag.BoolVar(&c.UDPRelay, "udprelay", false, "relay udp packets")
+		flag.BoolVar(&c.UDPOverTCP, "udpovertcp", false, "relay udp packets through tcp connection")
+		flag.BoolVar(&c.Mux, "mux", false, "use mux to reduce the number of connections")
+		flag.StringVar(&c.Nickname, "name", "", "nickname for logging")
+		flag.StringVar(&c.LogFile, "log", "", "set the path of logfile")
+		flag.BoolVar(&c.Verbose, "verbose", false, "show verbose log")
+		flag.BoolVar(&c.Debug, "debug", false, "show debug log")
+		flag.Parse()
+		if len(os.Args) == 1 {
+			flag.Usage()
+			return
+		}
+		if len(target) != 0 {
+			c.Backend = &ss.Config{Method: c.Method, Password: c.Password, Remoteaddr: c.Remoteaddr}
+			c.Remoteaddr = target
+		}
+		ss.CheckConfig(&c)
+		runServer(&c)
 		return
 	}
-	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime | log.Lmicroseconds)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -35,52 +62,7 @@ func main() {
 			go func(c *ss.Config) {
 				defer wg.Done()
 				c.Die = die
-				switch c.Type {
-				default:
-					log.Println("unsupported server type")
-				case "local":
-					c.Log("run client at", c.Localaddr, "with method", c.Method)
-					if c.UDPRelay {
-						c.Log("run udp local server at", c.Localaddr, "with method", c.Method)
-						go RunUDPLocalServer(c)
-					}
-					RunTCPLocalServer(c)
-				case "redir":
-					c.Log("run redir at", c.Localaddr, "with method", c.Method)
-					RunTCPRedirServer(c)
-				case "server":
-					c.Log("run server at", c.Localaddr, "with method", c.Method)
-					if c.UDPRelay {
-						c.Log("run udp remote server at", c.Localaddr, "with method", c.Method)
-						go RunUDPRemoteServer(c)
-					}
-					RunTCPRemoteServer(c)
-				case "multiserver":
-					c.Log("run multi server at", c.Localaddr)
-					if c.UDPRelay {
-						c.Log("run multi udp remote server at", c.Localaddr)
-						go RunMultiUDPRemoteServer(c)
-					}
-					RunMultiTCPRemoteServer(c)
-				case "ssproxy":
-					c.Log("run ss proxy at", c.Localaddr, "with method", c.Method)
-					RunSSProxyServer(c)
-				case "socksproxy":
-					c.Log("run socks proxy at", c.Localaddr, "with method", c.Method)
-					RunSocksProxyServer(c)
-				case "tcptun":
-					if len(c.Localaddr) == 0 || c.Backend == nil || len(c.Backend.Remoteaddr) == 0 {
-						break
-					}
-					c.Log("run tcp tunnel at", c.Localaddr, "to", c.Remoteaddr)
-					RunTCPTunServer(c)
-				case "udptun":
-					if len(c.Localaddr) == 0 || c.Backend == nil || len(c.Backend.Remoteaddr) == 0 {
-						break
-					}
-					c.Log("run udp tunnel at", c.Localaddr, "to", c.Remoteaddr)
-					RunUDPTunServer(c)
-				}
+				runServer(c)
 			}(c)
 		}
 		go func() {
@@ -121,5 +103,54 @@ func main() {
 		for _, c := range configs {
 			c.Close()
 		}
+	}
+}
+
+func runServer(c *ss.Config) {
+	switch c.Type {
+	default:
+		log.Println("unsupported server type")
+	case "local":
+		c.Log("run client at", c.Localaddr, "with method", c.Method)
+		if c.UDPRelay {
+			c.Log("run udp local server at", c.Localaddr, "with method", c.Method)
+			go RunUDPLocalServer(c)
+		}
+		RunTCPLocalServer(c)
+	case "redir":
+		c.Log("run redir at", c.Localaddr, "with method", c.Method)
+		RunTCPRedirServer(c)
+	case "server":
+		c.Log("run server at", c.Localaddr, "with method", c.Method)
+		if c.UDPRelay {
+			c.Log("run udp remote server at", c.Localaddr, "with method", c.Method)
+			go RunUDPRemoteServer(c)
+		}
+		RunTCPRemoteServer(c)
+	case "multiserver":
+		c.Log("run multi server at", c.Localaddr)
+		if c.UDPRelay {
+			c.Log("run multi udp remote server at", c.Localaddr)
+			go RunMultiUDPRemoteServer(c)
+		}
+		RunMultiTCPRemoteServer(c)
+	case "ssproxy":
+		c.Log("run ss proxy at", c.Localaddr, "with method", c.Method)
+		RunSSProxyServer(c)
+	case "socksproxy":
+		c.Log("run socks proxy at", c.Localaddr, "with method", c.Method)
+		RunSocksProxyServer(c)
+	case "tcptun":
+		if len(c.Localaddr) == 0 || c.Backend == nil || len(c.Backend.Remoteaddr) == 0 {
+			break
+		}
+		c.Log("run tcp tunnel at", c.Localaddr, "to", c.Remoteaddr)
+		RunTCPTunServer(c)
+	case "udptun":
+		if len(c.Localaddr) == 0 || c.Backend == nil || len(c.Backend.Remoteaddr) == 0 {
+			break
+		}
+		c.Log("run udp tunnel at", c.Localaddr, "to", c.Remoteaddr)
+		RunUDPTunServer(c)
 	}
 }
